82481d1cd834222b818f499f8169e53852fc2776
kimitawanjohi <kimitawanjohi7923@gmail.com>
2022-05-30 10:42:16 +0300

    added authentication to the chat app

--- core/asgi.py
+++ core/asgi.py
@@ -8,6 +8,7 @@ https://docs.djangoproject.com/en/4.0/howto/deployment/asgi/
 """
 
 import os
+from channels.auth import AuthMiddlewareStack
 from channels.routing import ProtocolTypeRouter, URLRouter
 from django.core.asgi import get_asgi_application
 
@@ -17,7 +18,9 @@ os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'core.settings')
 
 application = ProtocolTypeRouter({
     'http': get_asgi_application(),
-    'websocket': URLRouter(
-        chat.routing.websocket_urlpatterns
+    'websocket': AuthMiddlewareStack(
+        URLRouter(
+            chat.routing.websocket_urlpatterns
+        )
     ),
 })
--- static/room.js
+++ static/room.js
@@ -35,6 +35,64 @@ chatMessageInput.onkeyup = function(e) {
 // clear the 'chatMessageInput' and forward the message
 chatMessageSend.onclick = function() {
     if (chatMessageInput.value.length === 0) return;
-    // TODO: forward the message to the WebSocket
+    chatSocket.send(JSON.stringify({
+        "message": chatMessageInput.value,
+    }));
     chatMessageInput.value = "";
 };
+
+
+let chatSocket = null;
+
+function connect() {
+    chatSocket = new WebSocket("ws://" + window.location.host + "/ws/" + roomName + "/");
+
+    chatSocket.onopen = function(e) {
+        console.log("Successfully connected to the WebSocket.");
+    }
+
+    chatSocket.onclose = function(e) {
+        console.log("WebSocket connection closed unexpectedly. Trying to reconnect in 2s...");
+        setTimeout(function() {
+            console.log("Reconnecting...");
+            connect();
+        }, 2000);
+    };
+
+    chatSocket.onmessage = function(e) {
+        const data = JSON.parse(e.data);
+        console.log(data);
+
+        switch (data.type) {
+            case "chat_message":
+                chatLog.value += data.user =": " + data.message + "\n";
+                break;
+            case "user_list":
+                for (let i = 0; i < data.users.length; i++) {
+                    onlineUsersSelectorAdd(data.users[i]);
+                }
+                break;
+            case "user_join":
+                chatLog.value += data.user + " joined the room.\n";
+                onlineUsersSelectorAdd(data.user);
+                break;
+            case "user_leave":
+                chatLog.value += data.user + " left the room.\n";
+                onlineUsersSelectorRemove(data.user);
+                break;
+            default:
+                console.error("Unknown message type!");
+                break;
+        }
+
+        // scroll 'chatLog' to the bottom
+        chatLog.scrollTop = chatLog.scrollHeight;
+    };
+
+    chatSocket.onerror = function(err) {
+        console.log("WebSocket encountered an error: " + err.message);
+        console.log("Closing the socket.");
+        chatSocket.close();
+    }
+}
+connect();
